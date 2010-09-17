7157c901f870fa3e878362ea14abadb04311b5d4
Justin Smith <justin@19fish.com>
2010-09-17 15:59:27 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Champax::Application.config.secret_token = '6f752d424b3a6620cff49e5ef9678969e3cc860dca05394d637d29665feeeb2beda4bfc073219ec8bda604e243554a22d6ceee3bf501fb0caf812b77dfe2ce40'
