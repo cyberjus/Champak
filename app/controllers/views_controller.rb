a732da80977eef6cbafe29348026ec61aa60c706
Justin Smith <justin@19fish.com>
2010-10-06 16:56:31 -0400

    Minor Changes

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,21 +1,18 @@
 class ViewsController < ApplicationController
   
-  def index
-  end
-  
   def by_category
     @name = params[:name].tr('-', ' ').sub(' Coupons','')
     @title = "#{@name} Coupons"
     category = Category.find_by_name(@name)
     @coupon_items = Coupon.where("category_id = ?", category.id).includes(:business).order('businesses.name, short_description')  
-    render 'index'
+    render 'coupon_list'
   end
   
   def by_town
     @name = params[:name].tr('-', ' ').sub(' Coupons','')
     @title = "#{@name} Coupons"
     @coupon_items = Coupon.find(:all, :conditions => ['businesses.town = ?',@name], :order => 'businesses.name, short_description', :joins => {:business =>{}} )
-    render 'index'
+    render 'coupon_list'
   end
   
   def coupon
