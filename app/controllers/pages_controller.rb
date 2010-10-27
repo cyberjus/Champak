66551dd3b970833fc356eaef60011ca6281b27cb
Justin Smith <justin@19fish.com>
2010-10-27 13:01:33 -0400

    Top Rated Coupons on Main Page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   
   def home
-    @featured_coupons = Coupon.active.where("featured = ?", true)    
+    @featured_coupons = Coupon.active.includes(:business).where("featured = ?", true)  
+    @top_rated_coupons = Coupon.active.includes(:business).where("total_ratings <> ?", 0).order('rating DESC').limit(5)
   end
   
 end
